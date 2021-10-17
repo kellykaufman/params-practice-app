22c6d24f53a4aed5bce581800db0f18f240fcc69
Kelly Kaufman <kellykaufman12@gmail.com>
2021-10-16 17:43:52 -0700

    Adds segment parameter (wildcard)

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,7 +1,13 @@
 class ParamsController < ApplicationController
+  # def update_phrase
+  #   puts "Enter a phrase: "
+  #   phrase = params["phrase"].upcase
+  #   render json: { message: phrase }
+  # end
+
   def update_phrase
     puts "Enter a phrase: "
-    phrase = params["phrase"].upcase
+    phrase = params["wildcard"].upcase
     render json: { message: phrase }
   end
 end
